167b4b751a79733e45519c8dbc4d86cd0585c62d
Vinícius Silveira <vinifs19@gmail.com>
2022-10-08 16:32:10 -0300

    Added logic for rolling the dice by pressing the button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,17 +43,27 @@ fun DiceRollerApp() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
         .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+        .wrapContentSize(Alignment.Center),
+
 ) {
+    var result by remember { mutableStateOf(1) }
+    var imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Roll")
+        Image(painter = painterResource(imageResource), contentDescription = "1")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(text = "Roll", fontSize = 32.sp)
             
         }
 
